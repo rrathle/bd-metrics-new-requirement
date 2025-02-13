764847f965a03140da0091ff7fbef51679cd71bf
Raphael Rathle <rararaphaelrathle@gmail.com>
2025-02-12 16:50:43 -0800

    finished

--- src/OrderProcessor.java
+++ src/OrderProcessor.java
@@ -1,3 +1,4 @@
+import com.amazonaws.services.cloudwatch.model.StandardUnit;
 
 /**
  * Class representing final state of the coding activity.
@@ -26,6 +27,7 @@ public class OrderProcessor {
      * @return True if the order was successfully processed, false otherwise.
      */
     public boolean processOrder(Order newOrder) {
+        final double startTime = System.currentTimeMillis();
         boolean success = false;
 
         try {
@@ -38,6 +40,9 @@ public class OrderProcessor {
             System.out.println("Error processing order " + newOrder.getOrderNumber());
         }
 
+        final double endTime = System.currentTimeMillis();
+
+        metricsPublisher.addMetric("ORDER_PROCESSING_TIMES", endTime - startTime, StandardUnit.Milliseconds);
         return success;
     }
 }
